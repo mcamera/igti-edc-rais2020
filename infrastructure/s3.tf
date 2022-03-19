580b3f1b4b48f444e3f2c4e14912525eb3643854
mcamera <mcamera.oliveira@gmail.com>
2022-03-19 14:48:39 -0300

    Add Terraform AWS Infrastructure

---
+++ infrastructure/s3.tf
@@ -0,0 +1,33 @@
+# Creates a S3 buckets on AWS
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "${var.base_bucket_name}-${var.environment}-${var.account_number}"
+  tags = {
+    Name        = "CAMERA"
+    Environment = "DEV"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+}
+
+resource "aws_s3_bucket_public_access_block" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "bucket" {
+  bucket = aws_s3_bucket.bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
---
+++ infrastructure/variables.tf
@@ -0,0 +1,17 @@
+# Terraform variables
+
+variable "base_bucket_name" {
+  default = "datalake-camera-rais2020-tf"
+}
+
+variable "environment" {
+  default = "dev"
+}
+
+variable "account_number" {
+  default = "127012818163"
+}
+
+variable "region_id" {
+  default = "us-east-1"
+}
