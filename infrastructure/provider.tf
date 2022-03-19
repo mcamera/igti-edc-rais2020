a51bff069f47fb60500b2477e8d20552f7169e54
mcamera <mcamera.oliveira@gmail.com>
2022-03-19 15:17:33 -0300

    Config Terraform state to a AWS bucket

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,3 +1,11 @@
 provider "aws" {
   region = var.region_id
 }
+
+terraform {
+  backend "s3" {
+    bucket = "datalake-camera"
+    key = "state/rais2020/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
